39f4457208eb2d36b23a3dfd3d86ce6aa3f95fc0
ramosyara <yara.fernanda.ptal@gmail.com>
2023-06-20 15:05:30 +0000

    salvando

---
+++ app/src/main/java/application/model/produto.java
@@ -0,0 +1,38 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private int id;
+    private String titulo;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+   
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
