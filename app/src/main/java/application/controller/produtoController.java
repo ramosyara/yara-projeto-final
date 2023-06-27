07f9b609efa52a447c7307a10c114a460f085bd6
ramosyara <yara.fernanda.ptal@gmail.com>
2023-06-27 15:12:23 +0000

    salvando

--- app/src/main/java/application/controller/produtoController.java
+++ app/src/main/java/application/controller/produtoController.java
@@ -4,22 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.produtoRepository;
+import application.model.ProdutoRepository;
 
-@Controller 
+@Controller
 @RequestMapping("/produto")
 public class produtoController {
-    @Autowired 
+    @Autowired
     private produtoRepository produtoRepo;
-    
-    @RequestMapping("/produto")
-    public String list (Model model) {
+
+    @RequestMapping("/list")
+    public String list(Model model) {
         model.addAttribute("produtos", produtoRepo.findAll());
         return "/produto/list";
     }
 
+    @RequestMapping("/insert")
     public String insert() {
         return "/produto/insert";
     }
-}
+    
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(
+    @RequestParam("titulo") String titulo, 
+    @RequestParam("descricao") String descricao) {
+    Produto produto = new Produto();
+    produto.setTitulo(titulo);
+    produto.setDescricao(descricao);
+
+    produto.Repo.save(produto);
+    return "redirect:/produto/list";
+    }
